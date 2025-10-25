7b10eed7028ddc2bbcedff2660cd2e0abfff07fa
Anand Nishad <112419322+anand129-dev@users.noreply.github.com>
2025-10-25 15:53:58 +0000

    Added comments to js org iife and arw function files

--- Javascript/ChaiAurJavascript/js-hindi-youtube-main/03_basics/03_arrow.js
+++ Javascript/ChaiAurJavascript/js-hindi-youtube-main/03_basics/03_arrow.js
@@ -1,54 +1,50 @@
+// this (used inside object)
 const user = {
     username: "hitesh",
     price: 999,
-
+    // this defines the context
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
         console.log(this);
     }
-
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage()    // log the message and the context
+// user.username = "sam"    // updates the context
+// user.welcomeMessage()    // log the updated message and context
 
-// console.log(this);
+// console.log(this);       // gives an empty object {} bcoz node env runs stand alone, but in browser it gives window object
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);      // gives undefined, since this keyword works only with objects
 // }
-
 // chai()
-
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);      // gives undefined, even in this type of declaration of function
 // }
 
+// Arrow function
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this);      // {} empty object
 }
-
-
 // chai()
+
+// Basic arrow function: () => {}
+    // You can also hold it inside a function name : const myfunction = (parameters/arguments) => {//code}
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
+// Implicit return function : remove {} & return keyword, assuming we have single line return ßtatement
 // const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
+// const addTwo = (num1, num2) =>  (num1 + num2)        // same thing, just using a paranthese
+// If {} curly braces is used, return keyword is required compulsory.
+const addTwo = (num1, num2) => ({username: "hitesh"})   // return an object without return keyword and with ()
 console.log(addTwo(3, 4))
 
-
 // const myArray = [2, 5, 3, 7, 8]
-
 // myArray.forEach()
--- Javascript/ChaiAurJavascript/js-hindi-youtube-main/03_basics/04_iife.js
+++ Javascript/ChaiAurJavascript/js-hindi-youtube-main/03_basics/04_iife.js
@@ -1,11 +1,14 @@
 // Immediately Invoked Function Expressions (IIFE)
+// This function is executed immediately ex. database conncetion
+// Although we can make normal function invoke immediately, but sometimes functions, variables, etc. pollute from global scope
 
-
+// named IIFE
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();   // semicolon is used to handle multiple IIFE
 
+// unnamed IIFE with parameter
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
